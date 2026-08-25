0fd764172427d2d5a087fd9cd33cae815ab3ac2f
carlosllerenatheshedco-beep <carlos.llerena.theshedco@gmail.com>
2026-08-25 18:50:47 +0200

    Add files via upload

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://cezgntxyurzgctwwvmib.supabase.co'
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'sb_publishable_bd2aLD19H3XlqK_yZ5p-rQ_gGP6mXtc'
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
